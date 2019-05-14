02e44a3efb0ae0b48a7b41334d798fa5b4f913af
lightingSummer <1356221057@qq.com>
2019-05-14 22:22:12 +0800

    2019/5/14

---
+++ LeetCode/src/LeetCode021.java
@@ -0,0 +1,52 @@
+/**
+ * @author ：summerGit
+ * @date ：2019/5/14 0014
+ * @description：
+ */
+public class LeetCode021 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    /**
+     * author: summerGit
+     * date: 2019/5/14 0014
+     * description:
+     * Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+     * <p>
+     * Example:
+     * <p>
+     * Input: 1->2->4, 1->3->4
+     * Output: 1->1->2->3->4->4
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode fakeHead = new ListNode(0);
+        ListNode cur = fakeHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        while (l1 != null) {
+            cur.next = l1;
+            l1 = l1.next;
+            cur = cur.next;
+        }
+        while (l2 != null) {
+            cur.next = l2;
+            l2 = l2.next;
+            cur = cur.next;
+        }
+        return fakeHead.next;
+    }
+}
---
+++ LeetCode/src/LeetCode028.java
@@ -0,0 +1,17 @@
+/**
+ * @author ：summerGit
+ * @date ：2019/5/14 0014
+ * @description：
+ */
+public class LeetCode028 {
+    /**
+     * author: summerGit
+     * date: 2019/5/14 0014
+     * description: Implement strStr().
+     *
+     * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+     */
+    public int strStr(String haystack, String needle) {
+        return 0;
+    }
+}
