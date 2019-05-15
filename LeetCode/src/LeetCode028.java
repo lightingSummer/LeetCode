30514fecfdd3e16c51a67e87df1f1683989c0344
lightingSummer <1356221057@qq.com>
2019-05-15 10:34:52 +0800

    2019/5/15

--- LeetCode/src/LeetCode028.java
+++ LeetCode/src/LeetCode028.java
@@ -5,11 +5,12 @@
  */
 public class LeetCode028 {
     /**
-     * author: summerGit
-     * date: 2019/5/14 0014
-     * description: Implement strStr().
-     *
-     * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+     * @author: lightingSummer
+     * @date: 2019/5/15 0015
+     * @description:
+     * @param haystack
+     * @param needle
+     * @return int
      */
     public int strStr(String haystack, String needle) {
         return 0;
