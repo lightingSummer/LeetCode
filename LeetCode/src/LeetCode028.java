e77dff2a1b4ce6133ee8b9f2793d91aa32caebbc
lightingSummer <1356221057@qq.com>
2019-05-29 22:32:59 +0800

    5/29

--- LeetCode/src/LeetCode028.java
+++ LeetCode/src/LeetCode028.java
@@ -7,12 +7,24 @@ public class LeetCode028 {
     /**
      * @author: lightingSummer
      * @date: 2019/5/15 0015
-     * @description:
+     * @description: 最优算法应该是KMS
      * @param haystack
      * @param needle
      * @return int
      */
     public int strStr(String haystack, String needle) {
-        return 0;
+        int l1=haystack.length();
+        int l2=needle.length();
+        if(l1<l2){
+            return -1;
+        }else if(l2==0){
+            return 0;
+        }
+        for(int i=0;i<=l1-l2;i++){
+            if(haystack.substring(i,i+l2).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
     }
 }
