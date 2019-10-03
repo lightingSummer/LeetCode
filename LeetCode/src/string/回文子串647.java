ffe1260f65d02593a76b12b1d4985a173300eef1
lightingSummer <1356221057@qq.com>
2019-10-03 17:46:57 +0800

    try using future to solve problem

--- LeetCode/src/string/回文子串647.java
+++ LeetCode/src/string/回文子串647.java
@@ -1,5 +1,9 @@
 package string;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author     ：lightingSummer
  * @date       ：2019/7/22 0022
@@ -22,5 +26,54 @@ public class 回文子串647 {
             left--;
             right++;
         }
+    }
+
+    public int countSubstrings1(String s){
+        Callable<Integer> callable1 = new Callable<Integer>() {
+            int cnt = 0;
+
+            @Override
+            public Integer call() throws Exception {
+                for (int i = 0; i < s.length(); i++) {
+                    cnt = extendSubstrings1(s, i, i, cnt);
+                }
+                return cnt;
+            }
+        };
+        FutureTask<Integer> task1 = new FutureTask<>(callable1);
+        new Thread(task1).start();
+        Callable<Integer> callable2 = new Callable<Integer>() {
+            int cnt = 0;
+
+            @Override
+            public Integer call() throws Exception {
+                for (int i = 0; i < s.length(); i++) {
+                    cnt = extendSubstrings1(s, i, i + 1, cnt);
+                }
+                return cnt;
+            }
+        };
+        FutureTask<Integer> task2 = new FutureTask<>(callable2);
+        new Thread(task2).start();
+        int val1 = 0;
+        int val2 = 0;
+        try {
+            val1 = task1.get();
+            val2 = task2.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return val1 + val2;
+    }
+
+    private int extendSubstrings1(String s, int left, int right, int cnt) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            cnt++;
+            left--;
+            right++;
+        }
+        return cnt;
     }
 }
